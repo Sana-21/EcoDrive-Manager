d338240de10b9f3389a2b405b260c05d3ba9a6dc
Sana Abid <82564648+Sana-21@users.noreply.github.com>
2024-01-09 18:50:33 +0500

    added login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Dashboard from "./screens/Dashboard/Dashboard";
 import LoginScreen from './screens/LoginScreen/Login_Screen';
 import WelcomeScreen from './screens/WelcomeScreen/Welcome_Screen';
@@ -7,8 +9,15 @@ import HomeScreen from './screens/HomeScreen/Home_Screen'
 //import LoginScreen from "./screens/Login Screen/Login_Screen";
 function App() {
 
- return <HomeScreen />;
- 
+ return(
+    <BrowserRouter>
+    <Routes>
+    <Route exact path="/" element ={<WelcomeScreen/>} />
+    <Route path="/home" element={<HomeScreen/>} />
+    <Route path="/login" element={<LoginScreen/>} />
+    </Routes>
+    </BrowserRouter>
+ );
 }
 
 export default App;
