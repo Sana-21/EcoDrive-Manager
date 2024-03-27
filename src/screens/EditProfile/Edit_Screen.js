218aef074ad47c56467910e1a8c2205f68f4f7c0
Sana Abid <82564648+Sana-21@users.noreply.github.com>
2024-03-27 14:30:36 +0500

    fixed edit issues

--- src/screens/EditProfile/Edit_Screen.js
+++ src/screens/EditProfile/Edit_Screen.js
@@ -21,160 +21,148 @@ function EditScreen() {
         }
     }, [dispatch, userId]);
 
-    console.log("Redux State:", { userId, userData });
+    const [formState, setFormState] = useState({
+        companyName: "",
+        officeLocation: "",
+        selectedFile: null,
+        selectedLocation: {
+            lat: 31.5204,
+            lng: 74.3587,
+        },
+        errorMessage: ""
+    });
+  
+
+    useEffect(() => {
+        // Update form state with fetched user data
+        setFormState(prevState => ({
+            ...prevState,
+            companyName: userData.manager.companyName || "",
+            officeLocation: userData.manager.officeLocation || ""
+        }));
+    },[]);
+
+    const handleFileChange = (e) => {
+        setFormState(prevState => ({
+            ...prevState,
+            selectedFile: e.target.files[0]
+        }));
+    };
+
+    const handleLocationChange = (location) => {
+        setFormState(prevState => ({
+            ...prevState,
+            officeLocation: location
+        }));
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const formData = new FormData();
+            formData.append("companyName", formState.companyName);
+            formData.append("officeLocation", formState.officeLocation);
+            formData.append("officeImage", formState.selectedFile);
+
+            const response = await axios.put(`http://localhost:3001/api/user/${userData.id}`, formData);
+
+            if (response.data.success) {
+                // Handle success
+            } else {
+                setFormState(prevState => ({
+                    ...prevState,
+                    errorMessage: response.data.message || 'Error during update process.'
+                }));
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            setFormState(prevState => ({
+                ...prevState,
+                errorMessage: error.response?.data?.message || 'Error during update process'
+            }));
+        }
+    };
+
     return (
-        <div>
-            <h1>Edit Screen</h1>
-            <p>User Email: {userData ? userData.manager.email : "Loading..."}</p>
-    </div>
-       
+        <div className="edit-bg">
+            <Header showButton={false} showOptions={true} />
+
+            <div className="edit-box">
+                <div className="edit-title">
+                    <h3>Edit Profile</h3>
+                </div>
+
+                <div className="edit-form">
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="company-name">Company Name</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                id="company-name"
+                                placeholder="Enter your company name"
+                                required
+                                value={formState.companyName}
+                                onChange={(e) => setFormState(prevState => ({
+                                    ...prevState,
+                                    companyName: e.target.value
+                                }))}
+                            />
+                        </div>
+    
+                        <Popup
+                            trigger={
+                                <div className="form-group office-location-group">
+                                    <label htmlFor="office-location">Office Location</label>
+                                    <div className="input-group">
+                                        <input
+                                            className="form-control office-location-input"
+                                            type="text"
+                                            id="office-location"
+                                            placeholder="Address"
+                                            required
+                                            value={formState.officeLocation}
+                                            readOnly 
+                                        />
+                                        <div className="location-button">
+                                            <img src={LocationIcon} alt="Location" />
+                                        </div>
+                                    </div>
+                                </div>
+                            }
+                            modal
+                            nested
+                           >
+                            {(close) => (
+                                <div className="popup">
+                                    <SearchLocationInput setSelectedLocation={handleLocationChange} setOfficeLocation={handleLocationChange} />
+                                    <MapComponent selectedLocation={formState.selectedLocation} />
+                                    <div className="btn-container">
+                                        <MainButton text="Update Location" onClick={() => { close(); }}/>
+                                    </div>
+                                </div>
+                            )}
+                        </Popup>
+                        <div className="form-group">
+                            <label htmlFor="office-image">Upload New Picture</label>
+                            <input
+                                type="file"
+                                accept="image/*"
+                                id="office-image"
+                                name="officeImage"
+                                onChange={handleFileChange}
+                            />
+                        </div>
+                        {formState.errorMessage && <div className="error-message">{formState.errorMessage}</div>}
+                        <div className="btn-container">
+                            <MainButton text="Save Changes" type="submit" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
-    // const [formState, setFormState] = useState({
-    //     companyName: "",
-    //     officeLocation: "",
-    //     selectedFile: null,
-    //     selectedLocation: {
-    //         lat: 31.5204,
-    //         lng: 74.3587,
-    //     },
-    //     errorMessage: ""
-    // });
-    
-    // useEffect(() => {
-    //   // Fetch user data when component mounts
-    //   dispatch(fetchUserData());
-    // }, [dispatch]);
-
-    // useEffect(() => {
-    //     // Update form state with fetched user data
-    //     setFormState(prevState => ({
-    //         ...prevState,
-    //         companyName: userData.companyName || "",
-    //         officeLocation: userData.officeLocation || ""
-    //     }));
-    // }, [userData]);
-
-    // const handleFileChange = (e) => {
-    //     setFormState(prevState => ({
-    //         ...prevState,
-    //         selectedFile: e.target.files[0]
-    //     }));
-    // };
-
-    // const handleLocationChange = (location) => {
-    //     setFormState(prevState => ({
-    //         ...prevState,
-    //         officeLocation: location
-    //     }));
-    // };
-
-    // const handleSubmit = async (event) => {
-    //     event.preventDefault();
-
-    //     try {
-    //         const formData = new FormData();
-    //         formData.append("companyName", formState.companyName);
-    //         formData.append("officeLocation", formState.officeLocation);
-    //         formData.append("officeImage", formState.selectedFile);
-
-    //         const response = await axios.put(`http://localhost:3001/api/user/${userData.id}`, formData);
-
-    //         if (response.data.success) {
-    //             // Handle success
-    //         } else {
-    //             setFormState(prevState => ({
-    //                 ...prevState,
-    //                 errorMessage: response.data.message || 'Error during update process.'
-    //             }));
-    //         }
-    //     } catch (error) {
-    //         console.error('Error:', error);
-    //         setFormState(prevState => ({
-    //             ...prevState,
-    //             errorMessage: error.response?.data?.message || 'Error during update process'
-    //         }));
-    //     }
-    // };
-
-    // return (
-    //     <div className="edit-bg">
-    //         <Header showButton={false} showOptions={true} />
-
-    //         <div className="edit-box">
-    //             <div className="edit-title">
-    //                 <h3>Edit Profile</h3>
-    //             </div>
-
-    //             <div className="edit-form">
-    //                 <form onSubmit={handleSubmit}>
-    //                     <div className="form-group">
-    //                         <label htmlFor="company-name">Company Name</label>
-    //                         <input
-    //                             className="form-control"
-    //                             type="text"
-    //                             id="company-name"
-    //                             placeholder="Enter your company name"
-    //                             required
-    //                             value={formState.companyName}
-    //                             onChange={(e) => setFormState(prevState => ({
-    //                                 ...prevState,
-    //                                 companyName: e.target.value
-    //                             }))}
-    //                         />
-    //                     </div>
-    
-    //                     <Popup
-    //                         trigger={
-    //                             <div className="form-group office-location-group">
-    //                                 <label htmlFor="office-location">Office Location</label>
-    //                                 <div className="input-group">
-    //                                     <input
-    //                                         className="form-control office-location-input"
-    //                                         type="text"
-    //                                         id="office-location"
-    //                                         placeholder="Address"
-    //                                         required
-    //                                         value={formState.officeLocation}
-    //                                         readOnly 
-    //                                     />
-    //                                     <div className="location-button">
-    //                                         <img src={LocationIcon} alt="Location" />
-    //                                     </div>
-    //                                 </div>
-    //                             </div>
-    //                         }
-    //                         modal
-    //                         nested
-    //                        >
-    //                         {(close) => (
-    //                             <div className="popup">
-    //                                 <SearchLocationInput setSelectedLocation={handleLocationChange} setOfficeLocation={handleLocationChange} />
-    //                                 <MapComponent selectedLocation={formState.selectedLocation} />
-    //                                 <div className="btn-container">
-    //                                     <MainButton text="Update Location" onClick={() => { close(); }}/>
-    //                                 </div>
-    //                             </div>
-    //                         )}
-    //                     </Popup>
-    //                     <div className="form-group">
-    //                         <label htmlFor="office-image">Upload New Picture</label>
-    //                         <input
-    //                             type="file"
-    //                             accept="image/*"
-    //                             id="office-image"
-    //                             name="officeImage"
-    //                             onChange={handleFileChange}
-    //                         />
-    //                     </div>
-    //                     {formState.errorMessage && <div className="error-message">{formState.errorMessage}</div>}
-    //                     <div className="btn-container">
-    //                         <MainButton text="Save Changes" type="submit" />
-    //                     </div>
-    //                 </form>
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
-}
+ }
 
 export default EditScreen;
